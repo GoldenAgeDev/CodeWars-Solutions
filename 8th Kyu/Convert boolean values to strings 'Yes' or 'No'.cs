0c463b949b31a5b58c47bd32b23b3702223d5366
Ernie Peterson <ernie.peterson05@gmail.com>
2020-12-03 16:04:53 -0500

    Create Convert boolean values to strings 'Yes' or 'No'.cs

---
+++ 8th Kyu/Convert boolean values to strings 'Yes' or 'No'.cs
@@ -0,0 +1,18 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+using System;
+
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    if (word == true)
+    {
+        return "Yes";
+    }
+    else
+    {
+        return "No";
+    }
+  }
+}
